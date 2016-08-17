c7078cd276cfc71c0bb5e25b37fde716b380f072
Jervin Cruz <jervincruz26@gmail.com>
2016-08-16 20:23:46 -0700

    Update BoardType.java

--- BoardType.java
+++ BoardType.java
@@ -2,13 +2,12 @@ import java.awt.Graphics;
 import java.awt.Shape;
 import java.util.ArrayList;
 
-/**
- * Sets the amount of stones per pit and draws the graphics
+/** @Decorator
+ * Sets the amount of stones per pit and draws the board
  */
-public interface BoardType 
-{
+
+public interface BoardType{
     public void setInitial(int i);
     public void draw(Graphics g);
 	public ArrayList<Shape> getList();
-        
 }
